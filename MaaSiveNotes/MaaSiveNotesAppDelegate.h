0e8d608ce34ff446279962adbea57b88b23494c2
Brandon Trebitowski <brandontreb@gmail.com>
2011-08-16 12:31:49 -0600

    Initial Commit

---
+++ MaaSiveNotes/MaaSiveNotesAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MaaSiveNotesAppDelegate.h
+//  MaaSiveNotes
+//
+//  Created by Brandon Trebitowski on 8/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MaaSiveNotesAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ MaaSiveNotes/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  MaaSiveNotes
+//
+//  Created by Brandon Trebitowski on 8/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
